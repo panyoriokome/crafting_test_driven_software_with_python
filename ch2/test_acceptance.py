692c2c3124579fe03fb7e12e1f40e8a9cdc34f18
Yusuke_Watanabe <yuu-watanabe@ois.okasan.co.jp>
2021-07-21 19:41:26 +0900

    chat appを追加

--- ch2/test_acceptance.py
+++ ch2/test_acceptance.py
@@ -7,3 +7,13 @@ class TestChatAcceptance:
         messages = user2.fetch_messages()
 
         assert messages == ["山田: こんにちは"]
+
+class TestChatClient:
+    def test_nickname(self):
+        client = ChatClient("User 1")
+
+        assert client.nickname == 'User 1'
+
+class ChatClient:
+    def __init__(self, nickname):
+        self.nickname = nickname
