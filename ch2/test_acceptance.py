e103418869604dcc239fa63164857b1df1b69605
Yusuke_Watanabe <yuu-watanabe@ois.okasan.co.jp>
2021-07-26 19:21:43 +0900

    dummyの追加まで対応

--- ch2/test_acceptance.py
+++ ch2/test_acceptance.py
@@ -1,7 +1,7 @@
 class TestChatAcceptance:
     def test_message_exchange(self):
         user1 = ChatClient("山田")
-        user2 = chatClient("佐藤")
+        user2 = ChatClient("佐藤")
 
         user1.send_message("こんにちは")
         messages = user2.fetch_messages()
@@ -14,6 +14,22 @@ class TestChatClient:
 
         assert client.nickname == 'User 1'
 
+    def test_send_message(self):
+        client = ChatClient("User 1")
+        client.connection = _DummyConnection()
+        sent_message = client.send_message("Hello World")
+
+        assert sent_message == "User 1: Hello World"
+
 class ChatClient:
     def __init__(self, nickname):
         self.nickname = nickname
+
+    def send_message(self, message):
+        sent_message = "{}: {}".format(self.nickname, message)
+        self.connection.broadcast(message)
+        return sent_message
+
+class _DummyConnection:
+    def broadcast(*args, **kwargs):
+        pass
